c50a90cab05869cd0cda14e22f1247963a49b118
yeyuzhou1985 <70307711+yeyuzhou1985@users.noreply.github.com>
2026-03-12 11:54:20 +0800

    Update consts.ts
    1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "宇宙";
+export const SITE_TITLE = "啊宙的世界";
 export const SITE_DESCRIPTION = "Welcome to my website!";
